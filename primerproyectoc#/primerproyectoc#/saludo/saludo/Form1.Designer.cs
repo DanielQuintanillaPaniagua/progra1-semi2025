3718ffd036ef86fa64d99ff5ee20f1cbaf9ba58a
DanielQuintanillaPaniagua <danielquintanillapaniagua@gmail.com>
2025-07-24 20:12:19 -0600

    se agrego una caja y unas etiquetas con un boton para hacer un saludo

---
+++ primerproyectoc#/primerproyectoc#/saludo/saludo/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace saludo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnSaludar = new System.Windows.Forms.Button();
+            this.lblSaludar = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(27, 72);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(56, 16);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(89, 69);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(169, 22);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // btnSaludar
+            // 
+            this.btnSaludar.Location = new System.Drawing.Point(153, 123);
+            this.btnSaludar.Name = "btnSaludar";
+            this.btnSaludar.Size = new System.Drawing.Size(105, 57);
+            this.btnSaludar.TabIndex = 2;
+            this.btnSaludar.Text = "Saludar";
+            this.btnSaludar.UseVisualStyleBackColor = true;
+            this.btnSaludar.Click += new System.EventHandler(this.btnSaludar_Click);
+            // 
+            // lblSaludar
+            // 
+            this.lblSaludar.AutoSize = true;
+            this.lblSaludar.Location = new System.Drawing.Point(27, 220);
+            this.lblSaludar.Name = "lblSaludar";
+            this.lblSaludar.Size = new System.Drawing.Size(36, 16);
+            this.lblSaludar.TabIndex = 3;
+            this.lblSaludar.Text = "Hola";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 282);
+            this.Controls.Add(this.lblSaludar);
+            this.Controls.Add(this.btnSaludar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnSaludar;
+        private System.Windows.Forms.Label lblSaludar;
+    }
+}
+
