2214475a459c91fc62d012b3b315dba93e902b46
DanielQuintanillaPaniagua <danielquintanillapaniagua@gmail.com>
2025-09-18 19:09:31 -0600

    base se datos

---
+++ primerproyectoc#/primerproyectoc#/base de datos/base de datos/Conexion1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data;
+
+namespace base_de_datos
+{
+    internal class Conexion
+    {
+        internal string administrarDatosAlumnos(string[] alumnos, string accion)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal DataSet obtenerDatos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
