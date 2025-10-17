207ddde0f599dfc1aa3e4007da2d39fd492a7a71
DanielQuintanillaPaniagua <danielquintanillapaniagua@gmail.com>
2025-10-16 20:39:41 -0600

    para el proyecto final es agrego las tablas departamento empleado supervisor turno horario targeta_rfi

--- fiexpress-main/fiexpress/obj/Release/net8.0/fiexpress.AssemblyInfo.cs
+++ fiexpress-main/fiexpress/obj/Release/net8.0/fiexpress.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("fiexpress")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f2e4ae15efdb9366343c9ec9505d1e8c3678656")]
 [assembly: System.Reflection.AssemblyProductAttribute("fiexpress")]
