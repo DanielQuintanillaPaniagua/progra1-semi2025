4591a6bee1385cb69fff33758045561efecbd4da
DanielQuintanillaPaniagua <danielquintanillapaniagua@gmail.com>
2025-10-21 20:05:09 -0600

    continuacion del trabajo en clase

--- webappacademica/webappacademica/obj/Release/net8.0/webappacademica.AssemblyInfo.cs
+++ webappacademica/webappacademica/obj/Release/net8.0/webappacademica.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("webappacademica")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d1b4f50b646382b72befa2a8930809b99514eae")]
 [assembly: System.Reflection.AssemblyProductAttribute("webappacademica")]
